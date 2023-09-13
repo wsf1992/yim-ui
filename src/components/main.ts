2d6e0b630ee31f758ed18d85b1ecd2f3ab0e5958
wsf1992 <741987899@qq.com>
2023-09-13 23:08:24 +0800

    12

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,9 +1,12 @@
-import YmButton from './Button/index.vue';
-import YmSelect from './Select/index.vue';
+import YmButton from "./Button/index.vue";
+import YmSelect from "./Select/index.vue";
 
-export default function install(Vue) {
+export default {
+  install: function (Vue) {
     Vue.component(YmButton.name, YmButton);
     Vue.component(YmSelect.name, YmSelect);
-}
+  },
+};
 
 export { YmButton };
+export { YmSelect };
