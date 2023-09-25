a983215da49116596ac435fedc223ba1e148e1f0
wsf1992 <741987899@qq.com>
2023-09-25 16:42:43 +0800

    fix: 17

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
-import ElementUI from "element-ui";
+import { Popover } from "element-ui";
 import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+Vue.use(Popover);
 
 import YmButton from "./Button/index.vue";
 import YmSelect from "./Select/index.vue";
